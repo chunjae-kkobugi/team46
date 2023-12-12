5c303eef64ddf5f3ce0ad7154d82484b171488e8
hansun321 <hansun321@gmail.com>
2023-12-12 10:51:41 +0900

    [♻️] 마이페이지

--- src/main/java/com/memomo/ctrl/MemberCtrl.java
+++ src/main/java/com/memomo/ctrl/MemberCtrl.java
@@ -1,5 +1,6 @@
 package com.memomo.ctrl;
 
+import com.memomo.dto.BoardPostDTO;
 import com.memomo.dto.MemberDTO;
 import com.memomo.entity.Member;
 import com.memomo.service.BoardService;
@@ -7,6 +8,7 @@ import com.memomo.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +28,7 @@ public class MemberCtrl {
 
     private final MemberService memberService;
     private final HttpSession session;
+    private final ModelMapper modelMapper;
 
     @GetMapping("login")
     public String login() {
@@ -48,6 +51,12 @@ public class MemberCtrl {
             rttr.addFlashAttribute("msg", "아이디 또는 비밀번호를 잘못 입력했습니다.");
             return "redirect:/member/login";
         }
+    }
+
+    @GetMapping("logout")
+    public String logout() {
+        session.invalidate();
+        return "/index";
     }
 
     @GetMapping("join")
@@ -79,13 +88,16 @@ public class MemberCtrl {
         //System.out.println("아이디 : " + id);
         //System.out.println("회원 정보 : " + memberService.memberDetail(id));
         String id = (String) session.getAttribute("sid");
-        model.addAttribute("member", memberService.memberDetail(id));
-
+        //memberService.
+        MemberDTO dto = modelMapper.map(memberService.memberDetail(id), MemberDTO.class);
+        //model.addAttribute("member", memberService.memberDetail(id));
+        System.out.println("멤버디티오 : " + dto);
+        model.addAttribute("member", dto);
         return "member/mypage";
     }
 
     @PostMapping("edit")
-    public String edit(Member member, Model model) {
+    public String edit(MemberDTO member, Model model) {
         //MemberDTO member = new MemberDTO();
         //System.out.println("아이디 : " + id);
         //System.out.println("회원 정보 : " + memberService.memberDetail(id));
