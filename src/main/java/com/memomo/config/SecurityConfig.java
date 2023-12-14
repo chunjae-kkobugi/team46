2a1a446863021a11185b535589de536c72b09808
hansun321 <hansun321@gmail.com>
2023-12-15 01:02:14 +0900

    [♻️] 스프링 시큐리티 회원가입

---
+++ src/main/java/com/memomo/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package com.memomo.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+
+@Log4j2
+@Configuration
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        //.requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/admin/**", "/lecture/**", "/teacher/**").permitAll() //예진 테스트중
+                        .requestMatchers("/", "/**", "/login", "/join_frm", "join_frm_u", "join_frm_t").permitAll()
+                        .anyRequest().authenticated());
+
+        http
+                .formLogin((formLogin) ->
+                        formLogin
+                                .loginPage("/login")
+                                .usernameParameter("id")
+                                .passwordParameter("pw")
+                                .failureUrl("/member/login/error")
+                                .defaultSuccessUrl("/member/loginPro")
+                );
+
+        http
+                .csrf((csrf) ->
+                        csrf.disable()
+                );
+
+        http
+                .logout((logout) ->
+                        logout
+                                .logoutUrl("/member/logout")
+                                .logoutSuccessUrl("/")
+                );
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+                PathRequest.toStaticResources().atCommonLocations());
+    }
+
+}
---
+++ src/main/java/com/memomo/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.memomo.service;
+
+import com.memomo.entity.Member;
+import com.memomo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        System.out.println("유저디테일 id : " + id);
+
+        Optional<Member> optionalMember = memberRepository.findById(id);
+
+        System.out.println("optionalMember : " + optionalMember);
+
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            return User.builder()
+                    .username(member.getId())
+                    .password(member.getPw())
+                    .roles(member.getRole().toString())
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("User not found with id: " + id);
+        }
+    }
+}
