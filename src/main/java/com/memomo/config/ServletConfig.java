3092c7a3f12c9d517c0d8a511c43b2ca3d5eb576
hansun321 <hansun321@gmail.com>
2023-12-15 00:54:51 +0900

    [♻️] 스프링 시큐리티 적용

--- src/main/java/com/memomo/config/ServletConfig.java
+++ src/main/java/com/memomo/config/ServletConfig.java
@@ -1,9 +1,11 @@
 package com.memomo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableWebMvc
--- src/main/java/com/memomo/ctrl/MemberCtrl.java
+++ src/main/java/com/memomo/ctrl/MemberCtrl.java
@@ -1,25 +1,19 @@
 package com.memomo.ctrl;
 
-import com.memomo.dto.BoardPostDTO;
-import com.memomo.dto.MemberDTO;
+import com.memomo.dto.MemberFormDTO;
 import com.memomo.entity.Member;
-import com.memomo.service.BoardService;
 import com.memomo.service.MemberService;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @Log4j2
 @Controller
@@ -28,111 +22,52 @@ import java.io.PrintWriter;
 public class MemberCtrl {
 
     private final MemberService memberService;
-    private final HttpSession session;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    @GetMapping("login")
-    public String login() {
+    @GetMapping("/login")
+    public String loginMember() {
         return "member/login";
     }
 
-    @PostMapping("loginPro")
-    public String loginPro(String id, String pw, Model model, RedirectAttributes rttr) {
-        //int pass = memberService.loginPro(id, pw);
-        //System.out.println(id + " " + pw + " " + model);
-
-        String result = memberService.login(id, pw);
-        System.out.println("result : " + result);
-        if (result.equals("Login Success")) {
-            System.out.println("로그인 성공");
-            session.setAttribute("sid", id);
-            return "redirect:/";
-        } else {
-            System.out.println("로그인 실패");
-            rttr.addFlashAttribute("msg", "아이디 또는 비밀번호를 잘못 입력했습니다.");
-            return "redirect:/member/login";
-        }
+    @GetMapping("/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "member/login";
     }
 
-    @GetMapping("logout")
-    public String logout() {
-        session.invalidate();
-        return "/index";
+    @GetMapping("/loginPro")
+    public String loginPro() {
+        return "redirect:/";
     }
 
-    @GetMapping("join")
+    @GetMapping("/join")
     public String join(Model model) {
-        MemberDTO member = new MemberDTO();
+        MemberFormDTO member = new MemberFormDTO();
         model.addAttribute("member", member);
         return "member/join";
     }
 
-    @PostMapping("joinPro")
-    public String joinPro(MemberDTO member, Model model) {
-        //memberService.memberInsert(member);
-        memberService.join(member);
-        //model.addAttribute("msg", "가족이 되신걸 환영합니다.");
-        //model.addAttribute("url", "/member/login");
-        return "/member/login";
+    @PostMapping("/joinPro")
+    public String joinPro(@Valid MemberFormDTO memberFormDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "member/join";
+        }
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/join";
+        }
+        return "redirect:/";
     }
 
     @PostMapping("idCheckPro")
-    public ResponseEntity<Boolean> idCheck(@RequestBody MemberDTO member) throws Exception {
+    public ResponseEntity<Boolean> idCheck(@RequestBody MemberFormDTO member) throws Exception {
         String id = member.getId();
         boolean result = memberService.idCheck(id);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
-
-    @GetMapping("mypage")
-    public String myPage(Model model) {
-        //MemberDTO member = new MemberDTO();
-        //System.out.println("아이디 : " + id);
-        //System.out.println("회원 정보 : " + memberService.memberDetail(id));
-        String id = (String) session.getAttribute("sid");
-        //memberService.
-        MemberDTO dto = modelMapper.map(memberService.memberDetail(id), MemberDTO.class);
-        //model.addAttribute("member", memberService.memberDetail(id));
-        System.out.println("멤버디티오 : " + dto);
-        model.addAttribute("member", dto);
-        return "member/mypage";
-    }
-
-    @PostMapping("edit")
-    public String edit(MemberDTO member, Model model) {
-        //MemberDTO member = new MemberDTO();
-        //System.out.println("아이디 : " + id);
-        //System.out.println("회원 정보 : " + memberService.memberDetail(id));
-        //String id = (String) session.getAttribute("sid");
-        //model.addAttribute("member", memberService.memberDetail(id));
-        memberService.memberEdit(member);
-        return "member/mypage";
-    }
-
-    @GetMapping("remove")
-    public String remove(@RequestParam String id, HttpServletResponse response, Model model) {
-        System.out.println("탈퇴할 아이디 : " + id);
-
-        session.invalidate();
-        memberService.memberRemove(id);
-//        memberService.memberOutside(id);
-//        model.addAttribute("msg", "회원 탈퇴가 정상적으로 이루어졌습니다. 감사합니다.");
-//        model.addAttribute("url", "");
-        response.setContentType("text/html; charset=UTF-8");
-        PrintWriter out;
-        try {
-            out = response.getWriter();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        out.println("<script>");
-        out.println("회원 탈퇴가 정상적으로 이루어졌습니다. 감사합니다.");
-        out.println("</script>");
-        out.flush();
-
-
-
-        return "/index";
-    }
-
 
 }
