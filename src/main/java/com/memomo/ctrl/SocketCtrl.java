fa851da0d46dc90681e48e36bf364ee2111a252e
sangmin0816 <sangmin990816@gmail.com>
2023-12-16 23:27:22 +0900

    ✅ 포스트잇 수정 &  삭제 소켓 및 js 완료

--- src/main/java/com/memomo/ctrl/SocketCtrl.java
+++ src/main/java/com/memomo/ctrl/SocketCtrl.java
@@ -55,6 +55,7 @@ public class SocketCtrl {
         return "board/boardDetail";
     }
 
+    // 타임라인 테스트용
     @RequestMapping("/post/detail2")
     public String postEnter2(HttpServletRequest request, Model model){
         Integer bno = Integer.valueOf(request.getParameter("bno"));
@@ -63,6 +64,14 @@ public class SocketCtrl {
         for(PostDTO p:postList){
             plist2.add(p.getPno());
         }
+
+        plist = plist2;
+
+        Board board = boardService.boardDetail(bno);
+        model.addAttribute("detail", board);
+        model.addAttribute("postList", postList);
+        return "board/timeline";
+    }
 
 
     @MessageMapping("/remove/{bno}")
