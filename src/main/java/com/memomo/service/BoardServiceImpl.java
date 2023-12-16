279b163cdd7aaf8954578eb59f2a51d92197e537
emssme <bk75121@gmail.com>
2023-12-16 20:20:10 +0900

    [:bug:] 게시판 정보수정 오류 수정

--- src/main/java/com/memomo/service/BoardServiceImpl.java
+++ src/main/java/com/memomo/service/BoardServiceImpl.java
@@ -131,44 +131,93 @@ public class BoardServiceImpl implements BoardService{
         }
     }
 
-    @Override
     public void boardEdit(BoardDTO boardDTO, MultipartFile boardFile, HttpServletRequest request) {
 
-        // file upload
+        // 파일 업로드
         String imageOriginName = "";
         String imageSaveName = "";
 
         if (boardFile != null && !boardFile.isEmpty()) {
-            // 파일 업로드 로직은 이전과 동일
+            MultipartFile multipartFile = boardFile;
 
-            BoardFile existingFile = boardDTO.getFile(); // 실제 필드 이름으로 변경
+//            // 서버 경로
+//            ServletContext application = request.getSession().getServletContext();
+//            String uploadDir = application.getRealPath("/images/boardImage/");
+
+            // 로컬 경로
+            String uploadDir = "D:\\kim\\project\\tproj\\project06\\team46\\src\\main\\resources\\static\\images\\boardImage\\";
+
+            String today = new SimpleDateFormat("yyMMdd").format(new Date());
+            String saveFolder = uploadDir + today;
+            System.out.println(saveFolder);
+
+
+            File uploadPath = new File(saveFolder);
+            // 업로드 날짜의 폴더가 없다면 새로 생성
+            if (!uploadPath.exists()) {
+                uploadPath.mkdirs();
+            }
+
+            String originalName = boardFile.getOriginalFilename();
+            imageOriginName = originalName;
+
+            String uuid = UUID.randomUUID().toString();
+
+            String saveName = uuid + "_" + originalName;
+            imageSaveName = saveName;
+
+            String fileExtension = "";
+
+            // 파일 이름에 확장자가 있는지 확인
+            if (originalName != null) {
+                int lastIndex = originalName.lastIndexOf(".");
+                if (lastIndex != -1 && lastIndex < originalName.length() - 1) {
+                    fileExtension = originalName.substring(lastIndex + 1);
+                }
+            }
+
+            Path savePath = Paths.get(String.valueOf(uploadPath), saveName);
+            try {
+                multipartFile.transferTo(new File(uploadPath, saveName));
+                if (Files.probeContentType(savePath).startsWith("image")){
+                    File thumbnail = new File(uploadPath, "s_" + saveName);
+                    Thumbnailator.createThumbnail(savePath.toFile(), thumbnail, 411, 255);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("업로드 실패" + e.getMessage());
+            }
+            BoardFile fileResult = boardFileRepo.findBoardFileByFno(boardDTO.getBgImage());
+            fileResult.change("REMOVE");
+
+            BoardFile image = new BoardFile();
+            image.setBno(boardDTO.getBno());
+            image.setOriginName(imageOriginName);
+            image.setSaveName(imageSaveName);
+            image.setSavePath(today);
+            // 파일 확장자를 fileType 에 저장
+            image.setFileType(fileExtension);
+            image.setStatus("ACTIVE");
+
+            Optional<Board> result = boardRepo.findById(boardDTO.getBno());
+            Board board = result.orElseThrow();
+
+            BoardFile existingFile = boardDTO.getFile(); // 실제 필드명으로 변경
             if (existingFile != null) {
                 existingFile.setStatus("REMOVE");
             }
 
-            BoardFile image = new BoardFile();
-            // 새로운 파일 정보 설정
-
-            Optional<Board> result = boardRepo.findById(boardDTO.getBno());
-            Board board = result.orElseThrow();
-
             board.change(boardDTO.getTitle(), boardDTO.getBpw(), boardDTO.getMaxStudent(), boardDTO.getBgColor(), boardDTO.getBgImage());
+            // 이미지 정보를 게시판 정보에 연결
+            boardDTO.setFile(image);
+            boardRepo.save(board);
 
-            if (image != null) {
-                boardDTO.setFile(image);
-            }
-
+            // 파일 저장
             boardFileRepo.save(image);
             board.setBgImage(image.getFno());
             boardRepo.save(board);
-            log.info("이미지 fno: " + image.getFno());
-            log.info("게시판: " + board);
-        } else {
-            // 파일이 업로드되지 않은 경우의 로직
-            Optional<Board> result = boardRepo.findById(boardDTO.getBno());
-            Board board = result.orElseThrow();
-            board.change(boardDTO.getTitle(), boardDTO.getBpw(), boardDTO.getMaxStudent(), boardDTO.getBgColor(), boardDTO.getBgImage());
-            log.info("사진 수정X " + boardDTO);
+            log.info("-------------------------------------------------------------------------------" + image.getFno());
+            log.info("---------------------------------------------board : " + board);
         }
     }
 
