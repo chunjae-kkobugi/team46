6f7033a9370820b508fe7ecec0e688c9063c03b1
emssme <bk75121@gmail.com>
2023-12-18 15:32:47 +0900

    오류 수정

--- src/main/resources/static/js/postSocket.js
+++ src/main/resources/static/js/postSocket.js
@@ -114,70 +114,8 @@ function receiveSort(newOrder){
     }
 
 }
-
-function receiveAdd(newPost){
-    let p = JSON.parse(newPost);
-    // 문자열로 정의된 HTML 코드를 HTML 엘리먼트로 변환
-
-    let postT = `
-<li class="mb-3 mt-2 ui-sortable-handle ui-state-default" id="${p.pno}" data-pno="${p.pno}" style="width: 305px;">    
-    <div class="m-2">
-        <div class="card shadow-sm">
-            <!-- 포스트잇 내용-->
-            <div class="card-body" id="post${p.pno}" style="${(p.bgImage==null)? 'background-color: '+p.bgColor : null}">
-                <div class="original" id="original${p.pno}">
-                    <div id="postMenuList+${p.pno}" class="pe-2"
-                         style="position: absolute; right: 0; height: auto; z-index: 10; ">
-                        <ul class="p-0" style="list-style-type: none">
-
-                            <!-- 수정버튼 -->
-                            <li class="me-2" id="postModifyBtn${p.pno}"
-                                style="${(p.bgImage==null)? 'cursor: pointer; background-color : ' + p.bgColor : '#eeeeee'}">
-                                <span style="${(p.bgColor=='#ffffff' || p.bgColor == null) ? 'color : #333333' : 'mix-blend-mode: difference; color : #eeeeee'}">
-                                    <i class="fa-pen-to-square fa-solid"></i>
-                                </span>
-                            </li>
-
-                            <!-- 삭제 버튼 -->
-                            <li id="postRemoveBtn${p.pno}"
-                                style="${ p.bgImage==null ? 'cursor: pointer; background-color: ' + p.bgColor : '#eeeeee' }">
-                                <span style="${p.bgColor=='#ffffff' || p.bgColor==null ? 'color : #333333' : 'mix-blend-mode: difference; color : #eeeeee'}">
-                                    <i class="fa-solid fa-trash-can"></i></span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="text-body" style="height: 185px;">
-                        <p class="card-text pt-3" style="mix-blend-mode: difference; color: #eeeeee; font-size: 20px;">${p.content}</p>
-                    </div>
-                    <div class="d-flex justify-content-between row">
-                        <p class="card-text text-end mb-0" style="mix-blend-mode: exclusion; color: #ffffff">${p.author}</p>
-                    </div>
-                </div>
-
-                <!-- 포스트잇 수정 -->
-                <div class="modify register" id="modify${p.pno}">
-                    <form method="post" enctype="multipart/form-data" onsubmit="postEdit(this)">
-                        <div class="justify-content-center row">
-                            <textarea name="content" cols="30" rows="5" class="form-control" style="resize: none;height: 104px;">${p.content}</textarea>
-                            <input type="hidden" name="pno" id="${p.pno}" value="${p.pno}">
-                            <input type="hidden" name="bno" id="${p.bno}" value="${p.bno}">
-                            <div class="justify-content-between row">
-                                <input type="color" class="col-2 form-control mt-1" name="bgColor" value="${p.bgColor}">
-                                <input type="file" class="col-8 form-control mb-2 me-2 mt-2 uploadFiles" name="postFile" style="height: auto">
-                            </div>
-                            <div class="btn-group d-flex">
-                                <button type="button" class="btn btn-main" id="reset${p.pno}"> 취소 </button>
-                                <button type="submit" class="btn btn-main" id="modSubmit${p.pno}"> 수정 </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <!-- 포스트잇 수정 끝 -->
-            </div>
-        </div>
-    </div>
-</li>`;
-    console.log(postT)
+function receiveAdd(p){
+    let postT = postLayout(p);
     if(layoutNow === 'GRID'){
         $("#sortable").append(postT);
     }
@@ -202,5 +140,3 @@ window.onload = function (){
 window.BeforeUnloadEvent = function (){
     disconnect();
 }
-
-
