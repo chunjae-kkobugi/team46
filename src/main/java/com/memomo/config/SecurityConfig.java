349b417b37b9684d30199489b5ad91749be052e9
hansun321 <hansun321@gmail.com>
2023-12-20 11:33:06 +0900

    [🐛] navbar

--- src/main/java/com/memomo/config/SecurityConfig.java
+++ src/main/java/com/memomo/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.memomo.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,7 +33,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/css/**", "/images/**", "/js/**", "/plugins/**").permitAll()
                 //.requestMatchers("/admin/**", "/lecture/**", "/teacher/**").permitAll()
                 //.requestMatchers("/", "/**", "/login", "/join_frm", "join_frm_u", "join_frm_t").permitAll()
                 .requestMatchers("/", "/member/**", "/board/list", "/post/**", "/send-mail", "/confirm").permitAll()
@@ -53,5 +54,11 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/")
         );
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().requestMatchers(
+                PathRequest.toStaticResources().atCommonLocations());
     }
 }
